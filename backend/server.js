565d078dfcb1dbaf4d8910652d1fbab6027cd361
akashsingh_bhandari <asbhandari14@gmail.com>
2024-11-09 19:20:30 +0530

    Add website to the deployment

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,11 @@ const authRouter = require("./routers/auth-router");
 const productRouter = require("./routers/product-router");
 const connectionDB = require("./Database/connectionDB");
 const cors = require("cors");
+const path = require("path");
 
 const PORT = process.env.PORT;
+
+const dirname = path.resolve();
 
 
 app.use(cors({ origin : "http://localhost:5173" , methods: ['GET', 'POST', 'DELETE', 'PUT'], allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Expires", "Pragma"] , credentials: true}))
@@ -17,6 +20,12 @@ app.use(express.urlencoded({extended: true}));
 
 app.use("/auth", authRouter);
 app.use("/product", productRouter);
+
+
+app.use(express.static(path.join(dirname, "/frontend/dist")));
+app.get("*", (req, res)=>{
+    res.sendFile(path.resolve(dirname, "frontend", "dist", "index.html"))
+})
 
 
 
